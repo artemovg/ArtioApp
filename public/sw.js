7249561101eefc9d012d7e2c422f6f307109466e
artemovg <71792987+artemovg@users.noreply.github.com>
2026-04-27 20:30:54 +0000

    sw changes for app install

---
+++ public/sw.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = "electrotool-v1";
+const assets = ["/"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
